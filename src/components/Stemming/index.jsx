bc9c5db36f1aab84b3232d6460c2a2564b0749c5
ladanov_n <ladanov_n@mail.ru>
2018-11-07 21:36:45 +0300

    stemming: added const

--- src/components/Stemming/index.jsx
+++ src/components/Stemming/index.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 
+class Porter {
+  constructor(words) {
+    this.words = words;
+  }
+}
+
 class Stemming extends Component {
   render() {
     return <section>Stemming</section>;
