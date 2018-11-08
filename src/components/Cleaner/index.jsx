7064dfaf68806a023ccb1845303fb5f9ce52a947
ladanov_n <ladanov_n@mail.ru>
2018-11-08 11:00:43 +0300

    fixed

--- src/components/Cleaner/index.jsx
+++ src/components/Cleaner/index.jsx
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
 import { Form, TextArea, Input } from "semantic-ui-react";
 
+function clearText(text, stopSymbols) {
+  console.log(text);
+  return text
+    .split("")
+    .map(symbol => {
+      if (stopSymbols.includes(symbol)) return " ";
+      else return symbol;
+    })
+    .join("")
+    .replace(/\s+/g, " ")
+    .toLowerCase();
+}
+
 class Cleaner extends Component {
   componentWillMount() {
     const newState = JSON.parse(JSON.stringify(this.props.state));
-    newState.data.clearedText = newState.data.fullText;
-    let newClearedText = [...newState.data.clearedText].map(symbol => {
-      if (newState.data.stopSymbols.includes(symbol)) return " ";
-      else return symbol;
-    });
-    newState.data.clearedText = newClearedText
-      .join("")
-      .replace(/\s+/g, " ")
-      .toLowerCase();
+    newState.data.clearedText = clearText(
+      newState.data.fullText,
+      newState.data.stopSymbols
+    );
     this.props.changeState(newState);
   }
   render() {
@@ -23,27 +31,18 @@ class Cleaner extends Component {
           label="Стоп символы"
           value={this.props.state.data.stopSymbols}
           onChange={(_, event) => {
-            const stopSymbols = [...event.value];
             const newState = JSON.parse(JSON.stringify(this.props.state));
             newState.data.stopSymbols = event.value;
-            newState.data.clearedText = newState.data.fullText;
-            let newClearedText = [...newState.data.clearedText].map(symbol => {
-              if (stopSymbols.includes(symbol)) return " ";
-              else return symbol;
-            });
-            newState.data.clearedText = newClearedText
-              .join("")
-              .replace(/\s+/g, " ")
-              .toLowerCase();
+            newState.data.clearedText = clearText(newState.data.fullText, [
+              ...event.value
+            ]);
             this.props.changeState(newState);
           }}
         />
         <Form.Field
           control={TextArea}
           label="Очищенный текст"
-          value={
-            this.props.state.data.clearedText || this.props.state.data.fullText
-          }
+          value={this.props.state.data.clearedText}
           autoHeight
         />
       </Form>
--- src/components/Stemming/index.jsx
+++ src/components/Stemming/index.jsx
@@ -1,9 +1,33 @@
 import React, { Component } from "react";
+// import * as PORTER_CONST from "./const";
 
 // class Porter {
 //   constructor(words) {
 //     this.words = words;
 //   }
+
+//   start() {
+//     return this.words.map(word => {
+//       return this.stemWord(word);
+//     });
+//   }
+
+//   stemWord(word) {
+//     let stemWord = this.step_1(word);
+//     stemWord = this.step_2(stemWord);
+//     stemWord = this.step_3(stemWord);
+//     stemWord = this.step_4(stemWord);
+
+//     return stemWord;
+//   }
+
+//   step_1(word) {}
+
+//   step_2(word) {}
+
+//   step_3(word) {}
+
+//   step_4(word) {}
 // }
 
 class Stemming extends Component {
