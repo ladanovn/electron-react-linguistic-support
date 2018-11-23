7f1ec6c5c226e1652cb68d9ceaa4784b3113369d
ladanov_n <ladanov_n@mail.ru>
2018-11-23 13:45:52 +0300

    added stemmig

--- src/components/Spliter/index.jsx
+++ src/components/Spliter/index.jsx
@@ -1,33 +1,14 @@
 import React, { Component } from "react";
 import { Table, Form, Dropdown } from "semantic-ui-react";
 
-const typeSort = [
-  {
-    text: "Без сортировки",
-    value: "withoutSorting"
-  },
-  {
-    text: "По возростанию с начала",
-    value: "ascendingFromBeginning"
-  },
-  {
-    text: "По убыванию с начала",
-    value: "descendingFromBeginning"
-  },
-  {
-    text: "По возростанию с конца",
-    value: "asceningFromEnding"
-  },
-  {
-    text: "По убыванию с конца",
-    value: "descendingFromEnding"
-  }
-];
+import { dropdownOptions, sorting } from "../../libs/sort";
 
 class Spliter extends Component {
   componentDidMount() {
     const newState = JSON.parse(JSON.stringify(this.props.state));
-    newState.data.words = this.props.state.data.clearedText.split(" ");
+    newState.data.words = this.props.state.data.clearedText
+      .split(" ")
+      .filter(word => word !== "");
     this.props.changeState(newState);
   }
 
@@ -36,58 +17,12 @@ class Spliter extends Component {
       <Form>
         <Form.Field>
           <Dropdown
-            options={typeSort}
-            defaultValue={typeSort[0].value}
+            options={dropdownOptions}
+            defaultValue={dropdownOptions[0].value}
             onChange={(_, event) => {
               const newState = JSON.parse(JSON.stringify(this.props.state));
-              switch (event.value) {
-                case "ascendingFromBeginning":
-                  newState.data.words = this.props.state.data.words.sort(
-                    (a, b) => {
-                      return a.localeCompare(b);
-                    }
-                  );
-                  break;
-                case "descendingFromBeginning":
-                  newState.data.words = this.props.state.data.words.sort(
-                    (a, b) => {
-                      return b.localeCompare(a);
-                    }
-                  );
-                  break;
-                case "asceningFromEnding":
-                  newState.data.words = this.props.state.data.words.sort(
-                    (a, b) => {
-                      const rev_a = a
-                        .split("")
-                        .reverse()
-                        .join("");
-                      const rev_b = b
-                        .split("")
-                        .reverse()
-                        .join("");
-                      return rev_a.localeCompare(rev_b);
-                    }
-                  );
-                  break;
-                case "descendingFromEnding":
-                  newState.data.words = this.props.state.data.words.sort(
-                    (a, b) => {
-                      const rev_a = a
-                        .split("")
-                        .reverse()
-                        .join("");
-                      const rev_b = b
-                        .split("")
-                        .reverse()
-                        .join("");
-                      return rev_b.localeCompare(rev_a);
-                    }
-                  );
-                  break;
-                default:
-                  break;
-              }
+              newState.data.words = sorting[event.value](newState.data.words);
+
               this.props.changeState(newState);
             }}
           />
