b6f14b4aae4687c7f902e2efe160ec61b7f99a0c
ladanov_n <ladanov_n@mail.ru>
2018-11-26 12:36:08 +0300

    Updates

---
+++ precache-manifest.47ca6490e1bd30df5c3d41d528ca01cf.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6334816320d7b1d48a6",
+    "url": "/ladanovn/electron-react-linguistic-support/static/css/main.b35b730a.chunk.css"
+  },
+  {
+    "revision": "b6334816320d7b1d48a6",
+    "url": "/ladanovn/electron-react-linguistic-support/static/js/main.b6334816.chunk.js"
+  },
+  {
+    "revision": "73661568dd380f09d9df",
+    "url": "/ladanovn/electron-react-linguistic-support/static/css/1.b2912117.chunk.css"
+  },
+  {
+    "revision": "73661568dd380f09d9df",
+    "url": "/ladanovn/electron-react-linguistic-support/static/js/1.73661568.chunk.js"
+  },
+  {
+    "revision": "01569b21646ef8356fc9",
+    "url": "/ladanovn/electron-react-linguistic-support/static/js/runtime~main.01569b21.js"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/ladanovn/electron-react-linguistic-support/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/ladanovn/electron-react-linguistic-support/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/ladanovn/electron-react-linguistic-support/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/ladanovn/electron-react-linguistic-support/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/ladanovn/electron-react-linguistic-support/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/ladanovn/electron-react-linguistic-support/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/ladanovn/electron-react-linguistic-support/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/ladanovn/electron-react-linguistic-support/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/ladanovn/electron-react-linguistic-support/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/ladanovn/electron-react-linguistic-support/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/ladanovn/electron-react-linguistic-support/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/ladanovn/electron-react-linguistic-support/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/ladanovn/electron-react-linguistic-support/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/ladanovn/electron-react-linguistic-support/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/ladanovn/electron-react-linguistic-support/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/ladanovn/electron-react-linguistic-support/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "b4e848b296a01c8872f2a9e800d103ae",
+    "url": "/ladanovn/electron-react-linguistic-support/index.html"
+  }
+];
